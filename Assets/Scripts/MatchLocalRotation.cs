3239fa10620a39ff292534bb6c292487c9df19d6
Taro Omiya <taro.omiya@omiyagames.com>
2016-10-01 21:58:47 -0400

    Adding the rest of the project.

---
+++ Assets/Scripts/MatchLocalRotation.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MatchLocalRotation : MonoBehaviour
+{
+	public Transform matchRotation;
+	private Transform thisRotation;
+
+	// Use this for initialization
+	void Start ()
+	{
+		thisRotation = transform;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		thisRotation.localRotation = matchRotation.localRotation;
+	}
+}
